635ecf7db99e7c6018a4008a5ed9d4a6ca421638
frsiam <siam.frs@gmail.com>
2022-04-09 23:33:32 +0600

    authentication added for unauthorize user and protect order page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Notfound from './components/Notfound/Notfound';
+import Orders from './components/Orders/Orders';
 import Products from './components/Products/Products';
 import Register from './components/Register/Register';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -14,6 +16,11 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        }></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<Notfound></Notfound>}></Route>
